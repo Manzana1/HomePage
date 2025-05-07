a3464f0fe0266d9b278ca0aecaa0badde12d5814
Manzana1 <61259393+Manzana1@users.noreply.github.com>
2025-05-07 15:43:53 -0400

    Adding incons for the footer and working on the responsive display

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,33 @@ function Footer({ language }) {
   return (
     <footer id="contact">
       <p>© {new Date().getFullYear()} Mandana Pelletier</p>
-      <p>
+      <p className="contact-link">
         {language === "fr" ? (
-          <a href="mailto:mandana-6@live.ca">Me contacter</a>
+          <a href="mailto:mandana-6@live.ca">
+            Contact <br></br>
+            <img src="/mail.png" alt="email" className="email-icon" />
+          </a>
         ) : (
           <a href="mailto:mandana-6@live.ca">Contact</a>
         )}
+        ·
         <a
           href="https://www.linkedin.com/in/mandana-pelletier-10677292"
           target="_blank"
           rel="noopener noreferrer"
         >
-          LinkedIn
+          LinkedIn <br></br>
+          <img src="/linkedin.png" alt="LinkedIn" className="linkedin-icon" />
         </a>
         ·
         <a href="/resume-fr.pdf" target="_blank" rel="noopener noreferrer">
-          CV
+          CV<br></br>
+          <img
+            src="/external-link.png"
+            alt="external-link"
+            className="external-link-icon"
+            LAB
+          />
         </a>
       </p>
     </footer>
